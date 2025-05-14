6feff9383ac247d25f492c313d8fc8c26b0595f0
VİOS HOLDİNG LTD. <erollitex@gmail.com>
2025-05-15 00:40:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ API_ID = int(os.environ.get("API_ID", "20658336"))
 API_HASH = os.environ.get("API_HASH", "cedfb5fb4ffee7ecc746b28afc7925e3") 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "{BOT_TOKEN}")
 
+# Yönetici ve Kısıtlamalar
+SUDO = list(map(int, os.environ.get("SUDO", "5910057231").split()))
+BANNED_USERS = os.environ.get("BANNED_USERS", None)
+
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "MuratVio")
+SUPPORT_GRP = os.environ.get("SUPPORT_GRP", "Bot4Chan")
+
+
 # Bot Bilgileri
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "LunaGramBot") 
 BOT_NAME = os.environ.get("BOT_NAME", "ᴛʜᴇɴᴀ ᴀɪ")
